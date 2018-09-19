8d50785add7440399675c699bda8d916a79f0091
pavbox <pavboxdev@gmail.com>
2018-09-20 01:28:12 +0300

    added fast init all repos script

---
+++ init_repos.sh
@@ -0,0 +1,19 @@
+
+mkdir ../1st_course
+git clone git@github.com:amm-vsu-2015/1y1s_basic.git ../1st_course/1st_semester/pascal
+git clone git@github.com:amm-vsu-2015/1y2s_basis.git ../1st_course/2nd_semester/pascal
+
+mkdir ../2nd_course
+git clone git@github.com:amm-vsu-2015/2y1s_algorithms.git ../2nd_course/1st_semester/algorithms
+git clone git@github.com:amm-vsu-2015/2y2s_cpp.git ../2nd_course/2nd_semester/cpp
+
+mkdir ../3rd_course
+git clone git@github.com:amm-vsu-2015/3y1s_oop.git ../3rd_course/1st_semester/oop
+
+mkdir ../4th_course
+git clone git@github.com:amm-vsu-2015/4y1s_python.git ../4th_course/1st_semester/python
+git clone git@github.com:amm-vsu-2015/4y1s_python.git ../4th_course/1st_semester/java
+
+mkdir ../5th_course
+mkdir ../5th_course/1st_semester
+mkdir ../5th_course/2nd_semester
